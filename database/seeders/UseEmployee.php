30dce62ef9c4aaf641dbc4f2a8d1dd4380999fb0
JorgeOrlando512 <jorgino91.jh@gmail.com>
2022-06-30 23:14:00 -0500

    create employees seeder

---
+++ database/seeders/UseEmployee.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+
+class UseEmployee extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('employees')->insert([
+            'name' => 'Joe Doe',
+            'rol' => 'Administrator',
+            'age' => 47,
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+        DB::table('employees')->insert([
+            'name' => 'Jane Doe',
+            'rol' => 'Seller',
+            'age' => 25,
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+        DB::table('employees')->insert([
+            'name' => 'John Ambrose',
+            'rol' => 'Seller',
+            'age' => 30,
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+    }
+}
