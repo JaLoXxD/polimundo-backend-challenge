27e4b40620fbb189d09ab797d4be4bbcb42fee88
JorgeOrlando512 <jorgino91.jh@gmail.com>
2022-06-30 22:57:21 -0500

    create products seeder

---
+++ database/seeders/UseProduct.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+
+class UseProduct extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('products')->insert([
+            'name' => 'Graphic Card',
+            'type' => 'Nvidia RTX',
+            'stock' => 7,
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+        DB::table('products')->insert([
+            'name' => 'RAM Memory',
+            'type' => 'Corsair Vengeance',
+            'stock' => 15,
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+        DB::table('products')->insert([
+            'name' => 'Monitor',
+            'type' => 'Samsung Oddisey',
+            'stock' => 12,
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+        DB::table('products')->insert([
+            'name' => 'Microphone',
+            'type' => 'Blue Yeti',
+            'stock' => 20,
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+    }
+}
